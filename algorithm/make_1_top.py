541c6b97fac65fee4b8479ecc688adda6da51397
HyunWoo Oh <gusdn959@gmail.com>
2022-05-16 18:49:18 +0900

    commit

---
+++ algorithm/make_1_top.py
@@ -0,0 +1,26 @@
+N = int(input())
+d = [None] * 31
+def topdown(n: int, d: list[int]) -> int:
+    if n == 1:
+        d[1] = 0
+        return d[1]
+    else:
+        ### 이미 계산되어 있는 경우
+        if d[n] is not None:
+            return d[n]
+        ### 계산되어 있지 않은 경우
+        else:
+            answer = n
+            if n % 2 == 0:
+                answer = min(answer, topdown(n//2, d)+1)
+            if n % 3 == 0:
+                answer = min(answer, topdown(n//3, d)+1)
+            if n % 5 == 0:
+                answer = min(answer, topdown(n//5, d)+1)            
+            d[n] = answer
+            return d[n]
+
+
+
+print(topdown(30, d))
+
