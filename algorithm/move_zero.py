f7b7965c210487091cdb5345be0187857580d25a
HyunWoo Oh <gusdn959@gmail.com>
2022-05-31 21:18:56 +0900

    commit

---
+++ algorithm/move_zero.py
@@ -0,0 +1,33 @@
+def moveZeroes(nums: list[int]):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    zero = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[zero], nums[i] = nums[i], nums[zero]
+            zero += 1
+
+    return nums            
+
+
+def movezero(nums: list[int]):
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            now = i
+            next = now + 1
+            while next < len(nums):
+                if next < len(nums) and nums[next] != 0:
+                    nums[now], nums[next] = nums[next], nums[now]
+                    now = next
+                    next += 1
+                    continue
+                
+                elif next < len(nums) and nums[next] == 0:
+                    now += 1
+                    next = now + 1
+                    continue
+                
+
+
+
